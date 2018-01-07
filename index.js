3bf625b0adaefe8a39cdb7750abf4830320cedee
Kris Thompson <kt.devcat@gmail.com>
2018-01-07 18:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ class Route {
     this.endingLocation = endingLocation;
   }
 
-  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue',
-  'Park', 'Madison Avenue', '5th Avenue']
-
   blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue',
+    'Park', 'Madison Avenue', '5th Avenue']
+    
     const verticalDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    const horizontalDistance = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+
+    return verticalDistance + horizontalDistance;
   }
 }
