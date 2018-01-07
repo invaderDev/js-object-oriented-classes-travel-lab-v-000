25a3fd924528ac29c27d08aa6fef2d7bb9ab8cde
Kris Thompson <kt.devcat@gmail.com>
2018-01-07 18:44:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ class Route {
   estimatedTime() {
     let peak;
 
-    peak ? blocksTravelled() / 2 : blocksTravelled() / 3;
+    peak ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3;
   }
 }
